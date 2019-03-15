889cd15d1080d4bd4049fba01b9d84ef640ec45c
mvenkata11 <mvenkata11@gmail.com>
2019-03-15 13:04:17 -0700

    script to install terraform and docker on ubuntu16

---
+++ terraform_docker.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo " installing terraform"
+sudo curl -O https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip
+sudo apt-get install unzip
+sudo mkdir /bin/terraform 
+sudo unzip terraform_0.11.5_linux_amd64.zip -d /usr/local/bin/
+
+
+sleep 10 & echo" installing docker"
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get update
+sudo apt-get install docker-ce
