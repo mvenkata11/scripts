d4d5a41577f263eb112d97812f4e982a745ca733
mvenkata11 <mvenkata11@gmail.com>
2019-03-15 13:12:19 -0700

    added -y

--- terraform_docker.sh
+++ terraform_docker.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
+echo " installing git"
+sudo apt-get install git-core -y
 echo " installing terraform"
 sudo curl -O https://releases.hashicorp.com/terraform/0.11.5/terraform_0.11.5_linux_amd64.zip
-sudo apt-get install unzip
+sudo apt-get install unzip -y
 sudo mkdir /bin/terraform 
 sudo unzip terraform_0.11.5_linux_amd64.zip -d /usr/local/bin/
 
 
 sleep 10 & echo" installing docker"
-sudo apt-get update
-sudo apt-get install \
+sudo apt-get update -y
+sudo apt-get install -y\
     apt-transport-https \
     ca-certificates \
     curl \
@@ -20,5 +21,5 @@ sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
-sudo apt-get update
-sudo apt-get install docker-ce
+sudo apt-get update -y
+sudo apt-get install docker-ce -y
